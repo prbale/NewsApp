36df3255a8f011bdc703708aada26a70606e6b6e
Prashant <bale.prashant1984@gmail.com>
2022-03-16 08:00:30 +0530

    List Item updated

--- app/src/main/java/app/bale/newsapplication/ui/newsList/NewsAdapter.kt
+++ app/src/main/java/app/bale/newsapplication/ui/newsList/NewsAdapter.kt
@@ -1,12 +1,16 @@
 package app.bale.newsapplication.ui.newsList
 
+import android.os.Build
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
 import app.bale.newsapplication.data.model.Articles
 import app.bale.newsapplication.databinding.ItemBinding
 import app.bale.newsapplication.extension.loadImage
 import app.bale.newsapplication.listeners.OnItemClickListener
+import com.github.marlonlom.utilities.timeago.TimeAgo
+import java.time.Instant
 
 class NewsAdapter: RecyclerView.Adapter<MainViewHolder>() {
 
@@ -25,6 +29,7 @@ class NewsAdapter: RecyclerView.Adapter<MainViewHolder>() {
         return MainViewHolder(binding)
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
 
         val newsItem = news[position]
@@ -33,12 +38,20 @@ class NewsAdapter: RecyclerView.Adapter<MainViewHolder>() {
 
             newsImage.loadImage(newsItem.urlToImage)
             newsTitle.text = newsItem.title
-
+            newsDate.text = newsItem.publishedAt?.let { dateTimeAgo(it) }
+            newsDescription.text = newsItem.description
             root.setOnClickListener {
                 listener?.onItemClick(newsItem)
             }
         }
 
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun dateTimeAgo(date: String): String {
+        val instant = Instant.parse(date)
+        val ms = instant.toEpochMilli()
+        return TimeAgo.using(ms).replaceFirstChar { it.uppercaseChar() }
     }
 
     override fun getItemCount(): Int = news.size
